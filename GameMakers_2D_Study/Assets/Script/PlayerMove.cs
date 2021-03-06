dfd8baba3bab1fe7ab60087780424281ee78c0cd
husk <huskgit@gmail.com>
2021-03-06 20:59:55 +0900

    2021.03.06.Init

---
+++ GameMakers_2D_Study/Assets/Script/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour{
+    Rigidbody2D rigid;
+
+    public float maxSpeed;
+    public float jumpPower;
+    void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        // 버튼에서 손을 떼면 움직임 멈춤
+        if(Input.GetButtonUp("Horizontal")){
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.2f, rigid.velocity.y);
+        }
+
+        // 점프
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rigid.velocity = Vector2.up * jumpPower;
+        }
+    }
+    void FixedUpdate(){
+        // 좌우 이동
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+            // 최대 스피드 maxSpeed 를 넘지 못하게 함
+        if(rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if(rigid.velocity.x < -maxSpeed)
+            rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+    }
+}
