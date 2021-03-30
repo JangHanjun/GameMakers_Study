adfe6a0235c491ac07dd14a00b8aa795515748d7
husk <huskgit@gmail.com>
2021-03-30 22:24:38 +0900

    03.30

--- GameMakers_2D_Study/Assets/Script/PlayerMove.cs
+++ GameMakers_2D_Study/Assets/Script/PlayerMove.cs
@@ -7,18 +7,30 @@ public class PlayerMove : MonoBehaviour{
     SpriteRenderer spriteRenderer;
     Animator anim;
     public float maxSpeed;
+    // 점프 관련
     public float jumpPower;
     int jumpCount = 0;
-    public int maxJumpCount = 2;
+    public int maxJumpCount = 1;
     // 방향을 가져오기 위한 변수
     Vector3 dirVec;
     // 레이로 스캔하는 물체 받아오는 변수
     GameObject scanObject;
+    bool onWall;
+    bool isWallJump;
+    public float downSpeed;
+    public float wallJumpPower;
+    [SerializeField]
+    LayerMask ableClimgLayer;
+    // 스테미너 - 벽타기, 벽점프에 사용
+    [SerializeField]
+    private float playerStamina;
 
     void Awake() {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        // 스테미나 초기화
+        RefreshStamina();
     }
 
     void Update(){
@@ -27,7 +39,7 @@ public class PlayerMove : MonoBehaviour{
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.2f, rigid.velocity.y);
 
         // Flip
-        if (Input.GetButton("Horizontal"))
+        if (Input.GetButton("Horizontal") && !isWallJump)
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == 1;
         
         // 걷는 애니메이션
@@ -53,9 +65,11 @@ public class PlayerMove : MonoBehaviour{
     void FixedUpdate(){
         // 좌우 이동
         float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        if(!isWallJump){
+            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        }
 
-            // 최대 스피드 maxSpeed 를 넘지 못하게 함
+        // 최대 스피드 maxSpeed 를 넘지 못하게 함
         if(rigid.velocity.x > maxSpeed)
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if(rigid.velocity.x < -maxSpeed)
@@ -67,22 +81,54 @@ public class PlayerMove : MonoBehaviour{
             RaycastHit2D downRay = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Ground"));
 
             if(downRay.collider != null && downRay.distance < 0.5f){
-                Debug.Log("땅");
+                //Debug.Log("땅");
                 jumpCount = 0;
+                RefreshStamina();
             }
         }
 
-        // 조사를 하기 위한 Ray
-        // 씬 상에서 빨간색 선을 쏴서 레이에 닿으면 상호작용 가능하게 개조하면 됨
-        Debug.DrawRay(rigid.position, dirVec * 1.5f, new Color(1, 0, 0));
-        RaycastHit2D rayHit2 = Physics2D.Raycast(rigid.position, dirVec, 1.5f, LayerMask.GetMask("Object"));
+        // 벽타기용 Ray
+        Debug.DrawRay(rigid.position, dirVec * 0.4f, new Color(1, 0, 0));
+        RaycastHit2D rayHit2 = Physics2D.Raycast(rigid.position, dirVec, 0.4f, ableClimgLayer);
 
         if(rayHit2.collider != null){
-            scanObject = rayHit2.collider.gameObject;
+            onWall = true;
+
+            if(Input.GetKey(KeyCode.W) && playerStamina > 0){
+                //Debug.Log("climbing");
+                rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y + downSpeed);
+                playerStamina -= Time.deltaTime * 20;
+            } else {
+                rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * downSpeed);
+            }
+            
+
+            // wall jump
+            if(Input.GetAxis("Jump") != 0 && playerStamina > 0) {
+                isWallJump = true;
+                playerStamina -= 20;
+                Invoke("FreezX", 0.5f);
+                rigid.velocity = new Vector2(-0.9f * wallJumpPower, 0.9f * wallJumpPower);
+                if(spriteRenderer.flipX) {
+                    spriteRenderer.flipX = false;
+                } else if (!spriteRenderer.flipX) {
+                    spriteRenderer.flipX = true;
+                }
+            }
         }
         else{
-            scanObject = null;
+            onWall = false;
         }
         
+    }
+
+    void FreezX(){
+        isWallJump = false;
+        onWall = false;
+    }
+
+    // 스테미나 되돌리기
+    void RefreshStamina(){
+        playerStamina = 40;
     }
 }
