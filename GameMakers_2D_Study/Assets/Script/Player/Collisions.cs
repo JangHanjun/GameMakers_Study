97e0af0f77a9248093f89ad796dc140adcc1125b
husk <huskgit@gmail.com>
2021-04-04 22:02:31 +0900

    21.04.04

---
+++ GameMakers_2D_Study/Assets/Script/Player/Collisions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour{
+    [Header("Layers")]
+    public LayerMask groundLayer;
+    [Space]
+
+    public bool onGround;
+    public bool onWall;
+    public bool onRightWall;
+    public bool onLeftWall;
+    public int wallSide;
+
+    [Space]
+
+    [Header("Collision")]
+
+    public float collisionRadius = 0.5f;
+    public float collisionRadiusLR = 0.5f;
+    public Vector2 bottomOffset, rightOffset, leftOffset;
+    private Color debugCollisionColor = Color.red;
+
+    void Update(){  
+        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+        onWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadiusLR, groundLayer) 
+            || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadiusLR, groundLayer);
+
+        onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadiusLR, groundLayer);
+        onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadiusLR, groundLayer);
+
+        wallSide = onRightWall ? -1 : 1;
+    }
+
+    void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+
+        var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
+
+        Gizmos.DrawWireSphere((Vector2)transform.position  + bottomOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadiusLR);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadiusLR);
+    }
+}
