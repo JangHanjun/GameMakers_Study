8e4676b7ebbe10ef72fff4b665126e918337851d
husk <huskgit@gmail.com>
2021-05-09 11:50:52 +0900

    reflect

---
+++ GameMakers_2D_Study/Assets/Script/Light/moveex.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveex : MonoBehaviour{
+    Rigidbody2D rigid;
+    void Start(){
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.AddForce(new Vector2(9.8f * 25f, 9.8f * 25f));
+    }
+}
