79e5103c7daf5d1bf687e3c041caa59feebce2f5
husk <huskgit@gmail.com>
2021-05-16 16:53:43 +0900

    NPC_1

---
+++ GameMakers_2D_Study/Assets/Script/NPC/ChatSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChatSystem : MonoBehaviour{
+
+    public Queue<string> sentences;
+    public TextMeshPro text;
+    public GameObject quad;
+    string s;
+    float textW;
+    public void Ondialogue(string[] lines, Transform pos){
+        transform.position = pos.position;
+        sentences = new Queue<string>();
+        sentences.Clear();
+
+        foreach(var line in lines){
+            sentences.Enqueue(line);
+        }
+        // for(int i = 0; i < lines.Length; i++){
+        //     sentences.Enqueue();
+        // }
+        StartCoroutine(Talk(pos));
+    }
+
+    IEnumerator Talk(Transform quadPos){
+        yield return null;
+
+        while(sentences.Count > 0) {
+            text.text = sentences.Dequeue();
+
+            // 말풍선(quad) 크기 지정
+            textW = text.preferredWidth;
+            textW = (textW > 3) ? 3 : 3 + 0.3f;
+            quad.transform.localScale = new Vector2(textW, text.preferredHeight + 0.3f);
+
+            transform.position = new Vector2(quadPos.position.x, quadPos.position.y + text.preferredHeight/2);
+
+            yield return new WaitForSeconds(3f);
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ GameMakers_2D_Study/Assets/Script/NPC/NpcMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcMovement : MonoBehaviour{
+    public string[] sentences;
+    public Transform chatTransform;
+    public GameObject chatBox;
+
+    public void Talk(){
+        GameObject go = Instantiate(chatBox);
+        go.GetComponent<ChatSystem>().Ondialogue(sentences, chatTransform);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            Talk();
+        }
+    }
+}
