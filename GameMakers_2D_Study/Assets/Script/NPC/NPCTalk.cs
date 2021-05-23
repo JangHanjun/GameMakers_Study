12b21addee0d32d682c733942e22519a7f44b4ff
husk <huskgit@gmail.com>
2021-05-23 15:06:02 +0900

    NPC FSM_1

---
+++ GameMakers_2D_Study/Assets/Script/NPC/NPCTalk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTalk : MonoBehaviour{
+    
+    public string[] sentences;
+    public Transform chatTransform;
+    public GameObject chatBox;
+    RaycastHit2D rayHit;
+    public bool talking;
+    Rigidbody2D rigid;
+    SpriteRenderer sr;
+
+    private void Start() {
+        rigid = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void Talk(){
+        talking = true;
+        ChatSystem go = NpcDialogueMananger.NpcTalk(gameObject);
+        go.GetComponent<ChatSystem>().Ondialogue(sentences, chatTransform);
+        Invoke("Init", 8f);
+    }
+
+    void Init(){
+        talking = false;
+    }
+    
+}
---
+++ GameMakers_2D_Study/Assets/Script/NPC/NpcFsm.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcFsm : MonoBehaviour{
+
+    public enum NpcState { IDLE, TRACK, WARN }
+    public NpcState curState;
+    private Transform player;
+    private PlayerMovement playerDash;
+    private NPCTalk npcTalk;
+    WaitForSeconds delay = new WaitForSeconds(0.1f);
+    int randDir;
+    
+    void Start(){
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerDash = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        npcTalk = GetComponent<NPCTalk>();
+        curState = NpcState.IDLE;
+        StartCoroutine(IdleMove());
+    }
+
+    IEnumerator IdleMove(){
+        yield return delay;
+        if(playerDash.dashed){
+            Debug.Log("대쉬함");
+            StartCoroutine(DetectPlayer());
+            npcTalk.Talk();
+        } else
+            StartCoroutine(IdleMove());
+        // 근처에서 플레이어 감지를 하면
+        // curState = NpcState.TRACK;
+        // StartCoroutine(DetectPlayer());
+    }
+
+    IEnumerator DetectPlayer(){
+        Debug.Log("Track");
+        yield return delay;
+
+        
+    }
+
+    IEnumerator Warning(){
+        Debug.Log("Warn");
+        yield return delay;
+    }
+}
